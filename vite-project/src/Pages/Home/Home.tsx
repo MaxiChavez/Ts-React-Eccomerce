5216efedf2fdf40a178a5e56f06ffdd66e28f4e2
Maximiliano Chavez <104655710+Maxigamble@users.noreply.github.com>
2023-07-26 20:15:28 +0200

    Se crean las carpetas y se instalan dependecias

---
+++ vite-project/src/Pages/Home/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>soy home</div>;
+};
+
+export default Home;
--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "./index.css";
+
+//Rdx a la aplicacion
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+import "./App.css";
+
+const persistor = persistStore(store);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
