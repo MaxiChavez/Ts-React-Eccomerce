4c04e11da1977ce9dbcddc6c8d768b074c0d8209
Maximiliano Chavez <104655710+Maxigamble@users.noreply.github.com>
2023-08-11 10:02:27 +0200

    Se agrega funcionalidad al boton profile, si el usuario es admin lo redirige a vista admin, si no lo redirige a vista user

--- vite-project/src/Common/Header/Header.tsx
+++ vite-project/src/Common/Header/Header.tsx
@@ -89,7 +89,10 @@ export const Header = () => {
           <Navbar.Collapse id="navbarScroll">
             {location.pathname === "/" && (
               <>
-                <Form id="Searchh" className="d-flex mx-2 mx-auto mt-2 ">
+                <Form
+                  id="Searchh"
+                  className="d-flex mx-2 mx-lg-auto mt-2 mb-2 mb-lg-0"
+                >
                   <Form.Control
                     type="search"
                     placeholder="Search"
@@ -106,6 +109,7 @@ export const Header = () => {
                   aria-label="Default select example"
                   defaultValue={"0"}
                   onChange={handleCategoriaChange}
+                  className="mx-2 mt-2 mt-lg-0"
                 >
                   <option value="0" disabled>
                     Select categories
@@ -119,71 +123,69 @@ export const Header = () => {
               </>
             )}
 
-            <div id="logincart">
-              <div className="buttons-container">
-                <Nav
-                  className="d-flex"
-                  style={{ maxHeight: "100px" }}
-                  navbarScroll
-                >
-                  {isLogged ? (
-                    <>
+            <div id="logincart" className="mt-2 mt-lg-0">
+              <Nav className="d-flex" navbarScroll>
+                {isLogged ? (
+                  <>
+                    <Button
+                      className="mx-2 mt-2"
+                      variant="outline-dark"
+                      onClick={() =>
+                        userLogRd.user.id === 1
+                          ? navigate("/admin")
+                          : navigate("/user")
+                      }
+                    >
+                      Profile
+                    </Button>
+                    <Button
+                      className="mx-2 mt-2"
+                      variant="outline-dark"
+                      onClick={() => {
+                        dispatch(updateUser({ isLogged: false }));
+                        dispatch(updateUser({ user: {} }));
+                        dispatch(cleanCart());
+                        navigate("/");
+                      }}
+                    >
+                      Logout
+                    </Button>
+                    {isLogged && (
                       <Button
-                        className="mx-2 mt-2"
+                        id="btn-cart"
                         variant="outline-dark"
-                        onClick={() => navigate("/user")}
+                        onClick={() => navigate("/cart")}
+                        disabled={cantidadTotalCarrito === 0}
                       >
-                        Profile
-                      </Button>
-                      <Button
-                        className="mx-2 mt-2"
-                        variant="outline-dark"
-                        onClick={() => {
-                          dispatch(updateUser({ isLogged: false }));
-                          dispatch(updateUser({ user: {} }));
-                          dispatch(cleanCart());
-                          navigate("/");
-                        }}
-                      >
-                        Logout
-                      </Button>
-                      {isLogged && (
-                        <Button
-                          id="btn-cart"
-                          variant="outline-dark"
-                          onClick={() => navigate("/cart")}
-                          disabled={cantidadTotalCarrito === 0}
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-cart-fill ms-1"
+                          viewBox="0 0 18 18"
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
-                            fill="currentColor"
-                            className="bi bi-cart-fill ms-1"
-                            viewBox="0 0 18 18"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zm5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
-                            />
-                          </svg>
-                          ( {cantidadTotalCarrito} )
-                        </Button>
-                      )}
-                    </>
-                  ) : (
-                    <>
-                      <Button
-                        className="mx-2 mt-2"
-                        variant="outline-dark"
-                        onClick={() => navigate("/login")}
-                      >
-                        Login / Register
+                          <path
+                            fillRule="evenodd"
+                            d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zm5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"
+                          />
+                        </svg>
+                        ( {cantidadTotalCarrito} )
                       </Button>
-                    </>
-                  )}
-                </Nav>
-              </div>
+                    )}
+                  </>
+                ) : (
+                  <>
+                    <Button
+                      className="mx-2 mt-2"
+                      variant="outline-dark"
+                      onClick={() => navigate("/login")}
+                    >
+                      Login / Register
+                    </Button>
+                  </>
+                )}
+              </Nav>
             </div>
           </Navbar.Collapse>
         </Container>
